1626c3087a0f14ae27d57beb1458d022ce75dd6b
fabrimaida <fbrmaida@gmail.com>
2021-03-17 18:17:03 +0100

    Initial Authors Route

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+
+const bodyParser = require('body-parser')
+
+
+
+// 73CPhP5WvwHdmpTX
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -15,6 +22,7 @@ app.set('views', __dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit : '10mb',extended: false }))
 
 
 const mongoose = require('mongoose')
@@ -27,5 +35,7 @@ db.on('error', error => console.error(error))
 db.once('open',() => console.log('Connected to mongoose'))
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
+
 
 app.listen(process.env.PORT || 3000)
